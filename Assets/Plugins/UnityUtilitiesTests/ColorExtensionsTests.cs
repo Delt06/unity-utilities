0b51c86d5083bd8c63ed765d2527ee820d6f5a31
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-04 23:51:20 +0300

    Cleanup

--- Assets/Plugins/UnityUtilitiesTests/ColorExtensionsTests.cs
+++ Assets/Plugins/UnityUtilitiesTests/ColorExtensionsTests.cs
@@ -4,87 +4,65 @@ using UnityUtilities;
 
 namespace UnityUtilitiesTests
 {
-    [TestFixture]
-    public class ColorExtensionsTests
-    {
-        [Test]
-        [TestCase(1f, 1f, 1f, 1f)]
-        [TestCase(0f, 0f, 0f, 0f)]
-        [TestCase(0f, 0f, 0f, 1f)]
-        [TestCase(1/2f, 0f, 1/3f, 0f)]
-        [TestCase(1/1f, 1/5f, 1/5f, 0f)]
-        public void Color_Deconstruct_CorrectValues(float r, float g, float b, float a)
-        {
-            var color = new Color(r, g, b, a);
+	[TestFixture]
+	public class ColorExtensionsTests
+	{
+		[Test, TestCase(1f, 1f, 1f, 1f), TestCase(0f, 0f, 0f, 0f), TestCase(0f, 0f, 0f, 1f),
+		 TestCase(1 / 2f, 0f, 1 / 3f, 0f), TestCase(1 / 1f, 1 / 5f, 1 / 5f, 0f)]
+		public void Color_Deconstruct_CorrectValues(float r, float g, float b, float a)
+		{
+			var color = new Color(r, g, b, a);
 
-            var (rObtained, gObtained, bObtained, aObtained) = color;
-            
-            Assert.AreEqual(r, rObtained);
-            Assert.AreEqual(g, gObtained);
-            Assert.AreEqual(b, bObtained);
-            Assert.AreEqual(a, aObtained);
-        }
-        
-        [Test]
-        [TestCase(255, 255, 255, 255)]
-        [TestCase(0, 0, 0, 0)]
-        [TestCase(0, 0, 0, 255)]
-        [TestCase(33, 128, 0, 0)]
-        [TestCase(44, 55, 187, 1)]
-        public void Color32_Deconstruct_CorrectValues(byte r, byte g, byte b, byte a)
-        {
-            var color = new Color32(r, g, b, a);
+			var (rObtained, gObtained, bObtained, aObtained) = color;
 
-            var (rObtained, gObtained, bObtained, aObtained) = color;
-            
-            Assert.AreEqual(r, rObtained);
-            Assert.AreEqual(g, gObtained);
-            Assert.AreEqual(b, bObtained);
-            Assert.AreEqual(a, aObtained);
-        }
+			Assert.AreEqual(r, rObtained);
+			Assert.AreEqual(g, gObtained);
+			Assert.AreEqual(b, bObtained);
+			Assert.AreEqual(a, aObtained);
+		}
 
-        [Test]
-        [TestCase(null, null, null, null)]
-        [TestCase(null, null, null, 1f)]
-        [TestCase(1/2f, null, null, null)]
-        [TestCase(null, 1/3f, null, 0.5f)]
-        [TestCase(null, null, 1f, null)]
-        [TestCase(1/2f, 1/3f, null, null)]
-        [TestCase(null, 0f, 1/2f, null)]
-        [TestCase(1/2f, null, 3f, 1/3f)]
-        [TestCase(1/2f, 1/3f, 1/4f, 1/5f)]
-        public void Color_With_ValuesChange(float? r, float? g, float? b, float? a)
-        {
-            var originalVector = Color.white;
+		[Test, TestCase(255, 255, 255, 255), TestCase(0, 0, 0, 0), TestCase(0, 0, 0, 255), TestCase(33, 128, 0, 0),
+		 TestCase(44, 55, 187, 1)]
+		public void Color32_Deconstruct_CorrectValues(byte r, byte g, byte b, byte a)
+		{
+			var color = new Color32(r, g, b, a);
 
-            var newVector = originalVector.With(r, g, b, a);
-            
-            Assert.AreEqual(r ?? originalVector.r, newVector.r);
-            Assert.AreEqual(g ?? originalVector.g, newVector.g);
-            Assert.AreEqual(b ?? originalVector.b, newVector.b);
-            Assert.AreEqual(a ?? originalVector.a, newVector.a);
-        } 
-        
-        [Test]
-        [TestCase(null, null, null, null)]
-        [TestCase(null, null, null, 255)]
-        [TestCase(128, null, null, null)]
-        [TestCase(null, 64, null, 128)]
-        [TestCase(null, null, 255, null)]
-        [TestCase(127, 33, null, null)]
-        [TestCase(null, 13, 56, null)]
-        [TestCase(64, null, 33, 64)]
-        [TestCase(128, 44, 55, 32)]
-        public void Color32_With_ValuesChange(byte? r, byte? g, byte? b, byte? a)
-        {
-            var originalVector = new Color32(255, 255, 255, 255);
+			var (rObtained, gObtained, bObtained, aObtained) = color;
 
-            var newVector = originalVector.With(r, g, b, a);
-            
-            Assert.AreEqual(r ?? originalVector.r, newVector.r);
-            Assert.AreEqual(g ?? originalVector.g, newVector.g);
-            Assert.AreEqual(b ?? originalVector.b, newVector.b);
-            Assert.AreEqual(a ?? originalVector.a, newVector.a);
-        } 
-    }
+			Assert.AreEqual(r, rObtained);
+			Assert.AreEqual(g, gObtained);
+			Assert.AreEqual(b, bObtained);
+			Assert.AreEqual(a, aObtained);
+		}
+
+		[Test, TestCase(null, null, null, null), TestCase(null, null, null, 1f), TestCase(1 / 2f, null, null, null),
+		 TestCase(null, 1 / 3f, null, 0.5f), TestCase(null, null, 1f, null), TestCase(1 / 2f, 1 / 3f, null, null),
+		 TestCase(null, 0f, 1 / 2f, null), TestCase(1 / 2f, null, 3f, 1 / 3f), TestCase(1 / 2f, 1 / 3f, 1 / 4f, 1 / 5f)]
+		public void Color_With_ValuesChange(float? r, float? g, float? b, float? a)
+		{
+			var originalVector = Color.white;
+
+			var newVector = originalVector.With(r, g, b, a);
+
+			Assert.AreEqual(r ?? originalVector.r, newVector.r);
+			Assert.AreEqual(g ?? originalVector.g, newVector.g);
+			Assert.AreEqual(b ?? originalVector.b, newVector.b);
+			Assert.AreEqual(a ?? originalVector.a, newVector.a);
+		}
+
+		[Test, TestCase(null, null, null, null), TestCase(null, null, null, 255), TestCase(128, null, null, null),
+		 TestCase(null, 64, null, 128), TestCase(null, null, 255, null), TestCase(127, 33, null, null),
+		 TestCase(null, 13, 56, null), TestCase(64, null, 33, 64), TestCase(128, 44, 55, 32)]
+		public void Color32_With_ValuesChange(byte? r, byte? g, byte? b, byte? a)
+		{
+			var originalVector = new Color32(255, 255, 255, 255);
+
+			var newVector = originalVector.With(r, g, b, a);
+
+			Assert.AreEqual(r ?? originalVector.r, newVector.r);
+			Assert.AreEqual(g ?? originalVector.g, newVector.g);
+			Assert.AreEqual(b ?? originalVector.b, newVector.b);
+			Assert.AreEqual(a ?? originalVector.a, newVector.a);
+		}
+	}
 }
--- Assets/Plugins/UnityUtilitiesTests/VectorExtensionsTests.cs
+++ Assets/Plugins/UnityUtilitiesTests/VectorExtensionsTests.cs
@@ -4,81 +4,58 @@ using UnityUtilities;
 
 namespace UnityUtilitiesTests
 {
-    [TestFixture]
-    public class VectorExtensionsTests
-    {
-        [Test]
-        [TestCase(0f, 0f, 0f)]
-        [TestCase(0f, 0f, 1f)]
-        [TestCase(0f, 0f, -2f)]
-        [TestCase(0f, 2f, 0f)]
-        [TestCase(0f, -1f, 0f)]
-        [TestCase(5f, 0f, 0f)]
-        [TestCase(-3f, 0f, 0f)]
-        [TestCase(1f, 2f, 3f)]
-        [TestCase(-1f, 3f, 0f)]
-        public void Vector3_Deconstruct_CorrectValues(float x, float y, float z)
-        {
-            var vector = new Vector3(x, y, z);
-            
-            var (xOther, yOther, zOther) = vector;
-            
-            Assert.AreEqual(x, xOther);
-            Assert.AreEqual(y, yOther);
-            Assert.AreEqual(z, zOther);
-        }
-        
-        [Test]
-        [TestCase(0f, 0f)]
-        [TestCase(0f, 2f)]
-        [TestCase(0f, -1f)]
-        [TestCase(5f, 0f)]
-        [TestCase(-3f, 0f)]
-        [TestCase(1f, 2f)]
-        [TestCase(-1f, 3f)]
-        public void Vector2_Deconstruct_CorrectValues(float x, float y)
-        {
-            var vector = new Vector2(x, y);
-            
-            var (xOther, yOther) = vector;
-            
-            Assert.AreEqual(x, xOther);
-            Assert.AreEqual(y, yOther);
-        }
+	[TestFixture]
+	public class VectorExtensionsTests
+	{
+		[Test, TestCase(0f, 0f, 0f), TestCase(0f, 0f, 1f), TestCase(0f, 0f, -2f), TestCase(0f, 2f, 0f),
+		 TestCase(0f, -1f, 0f), TestCase(5f, 0f, 0f), TestCase(-3f, 0f, 0f), TestCase(1f, 2f, 3f),
+		 TestCase(-1f, 3f, 0f)]
+		public void Vector3_Deconstruct_CorrectValues(float x, float y, float z)
+		{
+			var vector = new Vector3(x, y, z);
 
-        [Test]
-        [TestCase(null, null, null)]
-        [TestCase(2f, null, null)]
-        [TestCase(null, 3f, null)]
-        [TestCase(null, null, -1f)]
-        [TestCase(2f, 3f, null)]
-        [TestCase(null, 0f, 2f)]
-        [TestCase(2f, null, 3f)]
-        [TestCase(2f, 3f, 4f)]
-        public void Vector3_With_ValuesChange(float? x, float? y, float? z)
-        {
-            var initialVector = Vector3.one;
-            
-            var (newX, newY, newZ) = initialVector.With(x, y, z);
-            
-            Assert.AreEqual(x ?? initialVector.x, newX);
-            Assert.AreEqual(y ?? initialVector.y, newY);
-            Assert.AreEqual(z ?? initialVector.z, newZ);
-        }
-        
-        [Test]
-        [TestCase(null, null)]
-        [TestCase(2f, null)]
-        [TestCase(null, 3f)]
-        [TestCase(2f, 3f)]
-        public void Vector2_With_ValuesChange(float? x, float? y)
-        {
-            var initialVector = Vector2.one;
-            
-            var (newX, newY) = initialVector.With(x, y);
-            
-            Assert.AreEqual(x ?? initialVector.x, newX);
-            Assert.AreEqual(y ?? initialVector.y, newY);
-        }
-    }
+			var (xOther, yOther, zOther) = vector;
+
+			Assert.AreEqual(x, xOther);
+			Assert.AreEqual(y, yOther);
+			Assert.AreEqual(z, zOther);
+		}
+
+		[Test, TestCase(0f, 0f), TestCase(0f, 2f), TestCase(0f, -1f), TestCase(5f, 0f), TestCase(-3f, 0f),
+		 TestCase(1f, 2f), TestCase(-1f, 3f)]
+		public void Vector2_Deconstruct_CorrectValues(float x, float y)
+		{
+			var vector = new Vector2(x, y);
+
+			var (xOther, yOther) = vector;
+
+			Assert.AreEqual(x, xOther);
+			Assert.AreEqual(y, yOther);
+		}
+
+		[Test, TestCase(null, null, null), TestCase(2f, null, null), TestCase(null, 3f, null),
+		 TestCase(null, null, -1f), TestCase(2f, 3f, null), TestCase(null, 0f, 2f), TestCase(2f, null, 3f),
+		 TestCase(2f, 3f, 4f)]
+		public void Vector3_With_ValuesChange(float? x, float? y, float? z)
+		{
+			var initialVector = Vector3.one;
+
+			var (newX, newY, newZ) = initialVector.With(x, y, z);
+
+			Assert.AreEqual(x ?? initialVector.x, newX);
+			Assert.AreEqual(y ?? initialVector.y, newY);
+			Assert.AreEqual(z ?? initialVector.z, newZ);
+		}
+
+		[Test, TestCase(null, null), TestCase(2f, null), TestCase(null, 3f), TestCase(2f, 3f)]
+		public void Vector2_With_ValuesChange(float? x, float? y)
+		{
+			var initialVector = Vector2.one;
+
+			var (newX, newY) = initialVector.With(x, y);
+
+			Assert.AreEqual(x ?? initialVector.x, newX);
+			Assert.AreEqual(y ?? initialVector.y, newY);
+		}
+	}
 }
